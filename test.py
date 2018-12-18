071e8a99e38e61dd0505ce4d64eb72cf8e4b5947
tao <dogfaraway@hotmail.com>
2018-12-18 21:17:49 +0800

    first push

--- test.py
+++ test.py
@@ -1,11 +1,3 @@
-import tensorflow as tf
-
-a = tf.random_normal((100, 100))
-b = tf.random_normal((100, 500))
-c = tf.matmul(a, b)  # 矩阵相乘
-sess = tf.InteractiveSession()
-sess.run(c)
-
 # coding:utf-8
 # 从tensorflow.examples.tutorials.mnist引入模块。这是TensorFlow为了教学MNIST而提前编制的程序
 from tensorflow.examples.tutorials.mnist import input_data
@@ -21,13 +13,22 @@ print(mnist.test.labels.shape)
 
 print(mnist.train.images[0, :])
 
+import numpy as np
+
+for i in range(20):
+    one_hot_label = mnist.train.labels[i, :]
+    label = np.argmax(one_hot_label)
+    print(label + 1)
+
+import tensorflow as tf
+
 x = tf.placeholder(tf.float32, [None, 784])
 W = tf.Variable(tf.zeros([784, 10]))
 b = tf.Variable(tf.zeros([10]))
 
 y = tf.nn.softmax(tf.matmul(x, W) + b)
 
-y_ = tf.placeholder(tf.float32, [None,10])
+y_ = tf.placeholder(tf.float32, [None, 10])
 
 cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y)))
 train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
@@ -36,10 +37,10 @@ tf.global_variables_initializer().run()
 
 for _ in range(1000):
     batch_xs, batch_ys = mnist.train.next_batch(100)
-    sess.run(train_step, feed_dict={x:batch_xs, y_:batch_ys})
+    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
 
-correct_prediction = tf.equal(tf.argmax(y,1),tf.argmax(y_,1))
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-print(sess.run(accuracy,feed_dict={x:mnist.test.images,y_:mnist.test.labels}))
+print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
 
 ##
