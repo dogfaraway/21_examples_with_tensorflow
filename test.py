d75f62099b789f7a4de8e1cafa2e8bcbcd5fa98c
tao <dogfaraway@hotmail.com>
2018-12-18 14:38:15 +0800

    first push

--- test.py
+++ test.py
@@ -41,3 +41,4 @@ for _ in range(1000):
 correct_prediction = tf.equal(tf.argmax(y,1),tf.argmax(y_,1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 print(sess.run(accuracy,feed_dict={x:mnist.test.images,y_:mnist.test.labels}))
+
