e2efd0d73b046fbc46991d7e995c67dd6b09378a
tao <dogfaraway@hotmail.com>
2018-12-18 14:23:50 +0800

    first push

---
+++ README.md
@@ -0,0 +1,2 @@
+"# Data-driven-prediction-models-of-energy-use-of-appliances-in-a-low-energy-house" 
+"# 21_examples_tensorflow" 
---
+++ test.py
@@ -0,0 +1,43 @@
+import tensorflow as tf
+
+a = tf.random_normal((100, 100))
+b = tf.random_normal((100, 500))
+c = tf.matmul(a, b)  # 矩阵相乘
+sess = tf.InteractiveSession()
+sess.run(c)
+
+# coding:utf-8
+# 从tensorflow.examples.tutorials.mnist引入模块。这是TensorFlow为了教学MNIST而提前编制的程序
+from tensorflow.examples.tutorials.mnist import input_data
+
+# 从MNIST_data/中读取MNIST数据。这条语句在数据不存在时，会自动执行下载
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+
+print(mnist.validation.images.shape)
+print(mnist.validation.labels.shape)
+
+print(mnist.test.images.shape)
+print(mnist.test.labels.shape)
+
+print(mnist.train.images[0, :])
+
+x = tf.placeholder(tf.float32, [None, 784])
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+
+y = tf.nn.softmax(tf.matmul(x, W) + b)
+
+y_ = tf.placeholder(tf.float32, [None,10])
+
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y)))
+train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
+sess = tf.InteractiveSession()
+tf.global_variables_initializer().run()
+
+for _ in range(1000):
+    batch_xs, batch_ys = mnist.train.next_batch(100)
+    sess.run(train_step, feed_dict={x:batch_xs, y_:batch_ys})
+
+correct_prediction = tf.equal(tf.argmax(y,1),tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+print(sess.run(accuracy,feed_dict={x:mnist.test.images,y_:mnist.test.labels}))
